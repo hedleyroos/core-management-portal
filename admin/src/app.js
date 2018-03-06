da87bf056c4e2125211af83da8e958cc90c44350
qoda <jpbydendyk@gmail.com>
2018-03-06 08:34:01 +0200

    Added new resources to app defn

--- admin/src/app.js
+++ admin/src/app.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import { jsonServerRestClient, Admin, Resource, fetchUtils } from 'admin-on-rest';
 
-import { DomainList } from './domains';
+import { DomainCreate, DomainEdit, DomainList, DomainShow } from './domains';
 import authClient from './authClient';
 
 
@@ -9,7 +9,7 @@ const App = () => (
     <Admin title='Girl Effect Management Portal'
             authClient={authClient}
             restClient={jsonServerRestClient('http://172.18.0.6:8000')}>
-        <Resource name="domains" list={DomainList} />
+        <Resource name="domains" list={DomainList} show={DomainShow} create={DomainCreate} edit={DomainEdit} />
     </Admin>
 );
 
