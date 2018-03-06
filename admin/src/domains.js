03f38d6f699aa61ddfb8f6b8409e5831d53a16a3
qoda <jpbydendyk@gmail.com>
2018-03-06 08:33:40 +0200

    Added create, show and edit resources for domains

--- admin/src/domains.js
+++ admin/src/domains.js
@@ -1,13 +1,67 @@
 import React from 'react';
-import { List, Datagrid, TextField } from 'admin-on-rest';
+import { CardActions } from 'material-ui/Card';
+import {
+    Create, CreateButton, Datagrid, DateField, DisabledInput, Edit, EditButton, Filter, List, RefreshButton, required, Show, ShowButton, SimpleForm,
+    SimpleShowLayout, TextField, TextInput, RichTextField
+} from 'admin-on-rest';
 
+
+const cardActionStyle = {
+    zIndex: 2,
+    display: 'inline-block',
+    float: 'right'
+};
+
+const DomainActions = ({ resource, filters, displayedFilters, filterValues, basePath, showFilter }) => (
+    <CardActions style={cardActionStyle}>
+        {filters && React.cloneElement(filters, { resource, showFilter, displayedFilters, filterValues, context: 'button' }) }
+        <CreateButton basePath={basePath} />
+        <RefreshButton />
+    </CardActions>
+);
+
+const DomainTitle = ({ record }) => {
+    return <span>Domain {record ? `'${record.name}'` : ''}</span>;
+};
 
 export const DomainList = (props) => (
-    <List {...props}>
+    <List actions={<DomainActions />} title='Domains' {...props}>
         <Datagrid>
-            <TextField source="id" />
-            <TextField source="name" />
-            <TextField source="description" />
+            <TextField source='id' />
+            <TextField source='name' />
+            <TextField source='description' />
+            <EditButton />
+            <ShowButton />
         </Datagrid>
     </List>
+);
+
+export const DomainShow = (props) => (
+    <Show title={<DomainTitle />} {...props}>
+        <SimpleShowLayout>
+            <DateField label='Created' source='created_at' />
+            <DateField label='Updated' source='updated_at' />
+            <TextField source='name' />
+            <RichTextField source='description' />
+        </SimpleShowLayout>
+    </Show>
+);
+
+export const DomainCreate = (props) => (
+    <Create {...props}>
+        <SimpleForm>
+            <TextInput source='name' validate={required} />
+            <TextInput source='description' validate={required} options={{ multiLine: true }} />
+        </SimpleForm>
+    </Create>
+);
+
+export const DomainEdit = (props) => (
+    <Edit title={<DomainTitle />} {...props}>
+        <SimpleForm>
+            <DisabledInput label="Id" source="id" />
+            <TextInput source="name" validate={required} />
+            <TextInput source='description' validate={required} options={{ multiLine: true }} />
+        </SimpleForm>
+    </Edit>
 );
