93f1239df995dc5f9165ff209867c5ce958e6e1f
qoda <jpbydendyk@gmail.com>
2018-03-08 14:47:26 +0200

    Added crud for sites

--- admin/src/app.js
+++ admin/src/app.js
@@ -1,14 +1,16 @@
 
 import React from 'react';
-import {pink500, pink900} from 'material-ui/styles/colors';
+import {pink500, pink300} from 'material-ui/styles/colors';
 import getMuiTheme from 'material-ui/styles/getMuiTheme';
 import DomainIcon from 'material-ui/svg-icons/action/language';
 import RoleIcon from 'material-ui/svg-icons/social/public';
+import SiteIcon from 'material-ui/svg-icons/action/explore';
 
-import { jsonServerRestClient, Admin, Delete, Resource, fetchUtils } from 'admin-on-rest';
+import { jsonServerRestClient, Admin, Delete, Resource } from 'admin-on-rest';
 
-import { DomainCreate, DomainEdit, DomainList, DomainRemove, DomainShow } from './domains';
-import { RoleCreate, RoleEdit, RoleList, RoleRemove, RoleShow } from './roles';
+import { DomainCreate, DomainEdit, DomainList, DomainShow } from './domains';
+import { RoleCreate, RoleEdit, RoleList, RoleShow } from './roles';
+import { SiteCreate, SiteEdit, SiteList, SiteShow } from './sites';
 import limitUpdateFields from './utils'
 
 
@@ -20,6 +22,7 @@ const App = () => (
     <Admin title='Girl Effect Management Portal' theme={getMuiTheme(muiTheme)} restClient={GEJsonServerRestClient}>
         <Resource name='domains' icon={DomainIcon} list={DomainList} show={DomainShow} create={DomainCreate} edit={DomainEdit} remove={Delete} options={{label: 'Domains'}} />
         <Resource name='roles' icon={RoleIcon} list={RoleList} show={RoleShow} create={RoleCreate} edit={RoleEdit} remove={Delete} options={{label: 'Roles'}} />
+        <Resource name='sites' icon={SiteIcon} list={SiteList} show={SiteShow} create={SiteCreate} edit={SiteEdit} remove={Delete} options={{label: 'Sites'}} />
     </Admin>
 );
 
@@ -27,7 +30,7 @@ const App = () => (
 const muiTheme = getMuiTheme({
     palette: {
         primary1Color: pink500,
-        accent1Color: pink900
+        accent1Color: pink300
     }
 });
 
