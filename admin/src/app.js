f19d33282068ce916d2d1b09a6aec6f98561a6e4
qoda <jpbydendyk@gmail.com>
2018-03-08 12:47:35 +0200

    Added pink theme to ui

--- admin/src/app.js
+++ admin/src/app.js
@@ -1,19 +1,35 @@
+
 import React from 'react';
-import { jsonServerRestClient, Admin, Delete, Resource, fetchUtils } from 'admin-on-rest';
+import {pink500, pink900} from 'material-ui/styles/colors';
+import getMuiTheme from 'material-ui/styles/getMuiTheme';
 import DomainIcon from 'material-ui/svg-icons/action/language';
+import RoleIcon from 'material-ui/svg-icons/social/public';
+
+import { jsonServerRestClient, Admin, Delete, Resource, fetchUtils } from 'admin-on-rest';
 
 import { DomainCreate, DomainEdit, DomainList, DomainRemove, DomainShow } from './domains';
+import { RoleCreate, RoleEdit, RoleList, RoleRemove, RoleShow } from './roles';
 import limitUpdateFields from './utils'
 
 
-const GEJsonServerRestClient = limitUpdateFields(jsonServerRestClient('http://core-management-layer:8000'))
+const GEJsonServerRestClient = limitUpdateFields(
+    jsonServerRestClient('http://core-management-layer:8000')
+)
 
 const App = () => (
-    <Admin title='Girl Effect Management Portal'
-            restClient={GEJsonServerRestClient}>
-        <Resource name='domains' icon={DomainIcon} list={DomainList} show={DomainShow} create={DomainCreate} edit={DomainEdit} remove={Delete} />
+    <Admin title='Girl Effect Management Portal' theme={getMuiTheme(muiTheme)} restClient={GEJsonServerRestClient}>
+        <Resource name='domains' icon={DomainIcon} list={DomainList} show={DomainShow} create={DomainCreate} edit={DomainEdit} remove={Delete} options={{label: 'Domains'}} />
+        <Resource name='roles' icon={RoleIcon} list={RoleList} show={RoleShow} create={RoleCreate} edit={RoleEdit} remove={Delete} options={{label: 'Roles'}} />
     </Admin>
 );
+
+
+const muiTheme = getMuiTheme({
+    palette: {
+        primary1Color: pink500,
+        accent1Color: pink900
+    }
+});
 
 
 export default App;
