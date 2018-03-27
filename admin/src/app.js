31d7de0b35981ef58a4e12b7bb1c444974c66f67
qoda <jpbydendyk@gmail.com>
2018-03-27 16:49:41 +0200

    Revert back to original authclient

--- admin/src/app.js
+++ admin/src/app.js
@@ -1,74 +1,31 @@
-import React from 'react';
-import PropTypes from 'prop-types';
-import { render } from 'react-dom';
 
-// redux, react-router, redux-form, saga, and material-ui
-// form the 'kernel' on which admin-on-rest runs
-import { combineReducers, createStore, compose, applyMiddleware } from 'redux';
-import { Provider } from 'react-redux';
-import createHistory from 'history/createHashHistory';
-import { Switch, Route } from 'react-router-dom';
-import { ConnectedRouter, routerReducer, routerMiddleware } from 'react-router-redux';
-import { reducer as formReducer } from 'redux-form';
-import createSagaMiddleware from 'redux-saga';
-import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
+import React from 'react';
 import {pink500, pink300} from 'material-ui/styles/colors';
 import getMuiTheme from 'material-ui/styles/getMuiTheme';
 import DomainIcon from 'material-ui/svg-icons/action/language';
 import RoleIcon from 'material-ui/svg-icons/social/public';
 import SiteIcon from 'material-ui/svg-icons/action/explore';
-import AppBar from 'material-ui/AppBar';
-import createOidcMiddleware, { createUserManager, OidcProvider } from 'redux-oidc';
-import { reducer as oidcReducer } from 'redux-oidc';
 
-// prebuilt admin-on-rest features
-import {
-    adminReducer,
-    localeReducer,
-    crudSaga,
-    simpleRestClient,
-    Delete,
-    TranslationProvider,
-    declareResources,
-} from 'admin-on-rest';
+import { Admin, Delete, Resource } from 'admin-on-rest';
 
-// your app components
 import { DomainCreate, DomainEdit, DomainList, DomainShow } from './domains';
+import { RoleCreate, RoleEdit, RoleList, RoleShow } from './roles';
+import { SiteCreate, SiteEdit, SiteList, SiteShow } from './sites';
+import { SiteRolesCreate, SiteRolesEdit, SiteRolesList, SiteRolesShow } from './siteroles';
+import swaggerRestServer from './swaggerRestServer';
+import authClient from './authClient';
+import authLogin from './authLogin';
 
-// user manager configuration object, see oidc-client-js documentation for details
-const config = {
-    client_id: 'my-client',
-    redirect_uri: `${window.location.protocol}//${window.location.hostname}:${window.location.port}/callback`,
-    response_type: 'id_token token',
-    scope: 'openid profile',
-    authority: 'http://myIdentityProvider.com',
-    post_logout_redirect_uri: `${window.location.protocol}//${window.location.hostname}:${window.location.port}/login`,
-    silent_redirect_uri: `${window.location.protocol}//${window.location.hostname}:${window.location.port}/silent_renew.html`,
-    automaticSilentRenew: true,
-    filterProtocolClaims: true,
-    loadUserInfo: true
-}
 
-// create a Redux app
-const reducer = combineReducers({
-    oidc: oidcReducer,
-    admin: adminReducer,
-    locale: localeReducer(),
-    form: formReducer,
-    routing: routerReducer,
-});
-const userManager = createUserManager(config);
-const sagaMiddleware = createSagaMiddleware();
-const oidcMiddleware = createOidcMiddleware(userManager, () => true, false, '/callback');
-const history = createHistory();
-const store = createStore(reducer, undefined, compose(
-    applyMiddleware(sagaMiddleware, routerMiddleware(history)),
-    applyMiddleware(oidcMiddleware),
-    window.devToolsExtension ? window.devToolsExtension() : f => f,
-));
-store.dispatch(declareResources([{ name: 'posts' }, { name: 'comments' }, { name: 'users' }]));
-const restClient = simpleRestClient('http://path.to.my.api/');
-sagaMiddleware.run(crudSaga(restClient));
+const App = () => (
+    <Admin title='Girl Effect Management Portal' theme={getMuiTheme(muiTheme)} restClient={swaggerRestServer('//core-management-layer:8000')} loginPage={authLogin} authClient={authClient}>
+        <Resource name='domains' icon={DomainIcon} list={DomainList} show={DomainShow} create={DomainCreate} edit={DomainEdit} remove={Delete} options={{label: 'Domains'}} />
+        <Resource name='roles' icon={RoleIcon} list={RoleList} show={RoleShow} create={RoleCreate} edit={RoleEdit} remove={Delete} options={{label: 'Roles'}} />
+        <Resource name='sites' icon={SiteIcon} list={SiteList} show={SiteShow} create={SiteCreate} edit={SiteEdit} remove={Delete} options={{label: 'Sites'}} />
+        <Resource name='siteroles' icon={SiteIcon} list={SiteRolesList} show={SiteRolesShow} create={SiteRolesCreate} edit={SiteRolesEdit} remove={Delete} options={{label: 'Site roles'}} />
+    </Admin>
+);
+
 
 const muiTheme = getMuiTheme({
     palette: {
@@ -77,23 +34,5 @@ const muiTheme = getMuiTheme({
     }
 });
 
-// bootstrap redux and the routes
-const App = () => (
-    <Provider store={store}>
-        <OidcProvider store={store} userManager={userManager}>
-            <ConnectedRouter history={history}>
-                <MuiThemeProvider>
-                    <AppBar title="My Admin" />
-                    <Switch>
-                        <Route exact path="/domains" hasCreate render={(routeProps) => <DomainList resource="domains" {...routeProps} />} />
-                        <Route exact path="/domains/create" render={(routeProps) => <DomainCreate resource="domains" {...routeProps} />} />
-                        <Route exact path="/domains/:id" hasShow hasDelete render={(routeProps) => <DomainEdit resource="domains" {...routeProps} />} />
-                        <Route exact path="/domains/:id/show" hasEdit render={(routeProps) => <DomainShow resource="domains" {...routeProps} />} />
-                    </Switch>
-                </MuiThemeProvider>
-            </ConnectedRouter>
-        </OidcProvider>
-    </Provider>
-);
 
 export default App;
