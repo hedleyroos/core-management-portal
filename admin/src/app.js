d70b0b8ab8cfcbd0dc693bc938e3fbb5a3e3690b
qoda <jpbydendyk@gmail.com>
2018-03-27 17:35:40 +0200

    Inject token header into all api calls

--- admin/src/app.js
+++ admin/src/app.js
@@ -6,7 +6,7 @@ import DomainIcon from 'material-ui/svg-icons/action/language';
 import RoleIcon from 'material-ui/svg-icons/social/public';
 import SiteIcon from 'material-ui/svg-icons/action/explore';
 
-import { Admin, Delete, Resource } from 'admin-on-rest';
+import { Admin, Delete, fetchUtils, Resource } from 'admin-on-rest';
 
 import { DomainCreate, DomainEdit, DomainList, DomainShow } from './domains';
 import { RoleCreate, RoleEdit, RoleList, RoleShow } from './roles';
@@ -15,9 +15,19 @@ import { SiteRolesCreate, SiteRolesEdit, SiteRolesList, SiteRolesShow } from './
 import swaggerRestServer from './swaggerRestServer';
 import authClient from './authClient';
 
+const httpClient = (url, options = {}) => {
+    if (!options.headers) {
+        options.headers = new Headers({ Accept: 'application/json' });
+    }
+    const id_token = localStorage.getItem('id_token');
+    options.headers.set('Authorization', `Bearer ${id_token}`);
+    return fetchUtils.fetchJson(url, options);
+}
+
+const restClient = swaggerRestServer('//core-management-layer:8000', httpClient)
 
 const App = () => (
-    <Admin title='Girl Effect Management Portal' theme={getMuiTheme(muiTheme)} restClient={swaggerRestServer('//core-management-layer:8000')} authClient={authClient}>
+    <Admin title='Girl Effect Management Portal' theme={getMuiTheme(muiTheme)} restClient={restClient} authClient={authClient}>
         <Resource name='domains' icon={DomainIcon} list={DomainList} show={DomainShow} create={DomainCreate} edit={DomainEdit} remove={Delete} options={{label: 'Domains'}} />
         <Resource name='roles' icon={RoleIcon} list={RoleList} show={RoleShow} create={RoleCreate} edit={RoleEdit} remove={Delete} options={{label: 'Roles'}} />
         <Resource name='sites' icon={SiteIcon} list={SiteList} show={SiteShow} create={SiteCreate} edit={SiteEdit} remove={Delete} options={{label: 'Sites'}} />
