2a74ed5572a13fe54a5db02816a82dc6f673e720
qoda <jpbydendyk@gmail.com>
2018-04-09 16:08:58 +0200

    Configured oidc provider urls

--- admin/src/authLogin.js
+++ admin/src/authLogin.js
@@ -11,6 +11,18 @@ import { pink500, pink300 } from 'material-ui/styles/colors';
 
 import { userLogin } from 'admin-on-rest';
 
+const OIDC_PROVIDER_URL = 'http://core-authentication-service:8000/login/?next=/openid/authorize/'
+const OIDC_PROVIDER_SCOPE = 'openid+profile+roles'
+const OIDC_PROVIDER_CLIENT_ID = 'management_portal'
+const OIDC_PROVIDER_REDIRECT_URI = encodeURIComponent('http://core-management-portal:3000/#/oidc/callback')
+const OIDC_PROVIDER_STATE = btoa(new Date());
+const OIDC_PROVIDER_NONCE = btoa(OIDC_PROVIDER_STATE);
+
+const oidc_query_string = encodeURIComponent(
+    `?scope=${OIDC_PROVIDER_SCOPE}&response_type=id_token+token&client_id=${OIDC_PROVIDER_CLIENT_ID}&redirect_uri=${OIDC_PROVIDER_REDIRECT_URI}&state=${OIDC_PROVIDER_STATE}&nonce=${OIDC_PROVIDER_NONCE}`
+)
+const login_url = `${OIDC_PROVIDER_URL}${oidc_query_string}`
+
 const styles = {
     main: {
         display: 'flex',
@@ -44,13 +56,16 @@ class AuthLoginPage extends Component {
                     <Card style={styles.card}>
                         <div style={styles.avatar}>
                             <Avatar
+                                backgroundColor={pink300}
                                 icon={<LockIcon />}
                                 size={60}
                             />
                         </div>
                         <CardActions>
+                        <p>Login with Girl Effect OIDC Provider</p>
                             <RaisedButton
-                                type="submit"
+                                type='button'
+                                href={login_url}
                                 primary
                                 label='Login'
                                 fullWidth
