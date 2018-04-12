43d13a4a8a33fbf9f7b3b4f65050563a5958bbbd
qoda <jpbydendyk@gmail.com>
2018-04-12 10:33:08 +0200

    Cleanup

--- admin/src/authClient.js
+++ admin/src/authClient.js
@@ -10,29 +10,6 @@ export default (type, params) => {
         localStorage.removeItem('id_token');
         localStorage.removeItem('refresh_token');
     }
-    // if (type === AUTH_ERROR) {
-    //     const refresh_token = localStorage.getItem('refresh_token');
-    //     const request = new Request(OPENID_PROVIDER_URL, {
-    //         method: 'POST',
-    //         body: queryString.stringify({
-    //             grant_type: 'refresh_token',
-    //             client_id: CLIENT_ID,
-    //             client_secret: CLIENT_SECRET,
-    //             refresh_token: refresh_token
-    //         }),
-    //         headers: {'Content-type': 'application/x-www-form-urlencoded'}
-    //     })
-    //     return fetch(request)
-    //         .then(response => {
-    //             if (response.status < 200 || response.status >= 300) {
-    //                 throw new Error(response.statusText);
-    //             }
-    //             return response.json();
-    //         })
-    //         .then(({ id_token }) => {
-    //             localStorage.setItem('id_token', id_token);
-    //         });
-    // }
     if (type === AUTH_CHECK) {
         return localStorage.getItem('id_token') ? Promise.resolve() : Promise.reject();
     }
