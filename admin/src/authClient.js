23e69d898dc7173c3179404d692e49e0e380c3fc
David Oberholzer <david.oberholzer@praekeltconsulting.com>
2018-04-16 15:59:12 +0200

    Added check for expired token and as a result, logout and remove the current token from localStorage.

--- admin/src/authClient.js
+++ admin/src/authClient.js
@@ -1,9 +1,16 @@
-import { AUTH_LOGOUT, AUTH_CHECK } from 'admin-on-rest';
+import { AUTH_LOGOUT, AUTH_CHECK, AUTH_ERROR } from 'admin-on-rest';
 
 export default (type, params) => {
     if (type === AUTH_LOGOUT) {
         localStorage.removeItem('id_token');
         localStorage.removeItem('auth_state');
+    }
+    if (type === AUTH_ERROR) {
+        if (params.message === 'Token expired') {
+            localStorage.removeItem('id_token');
+            return Promise.reject();
+        }
+        return Promise.resolve();
     }
     if (type === AUTH_CHECK) {
         return localStorage.getItem('id_token') ? Promise.resolve() : Promise.reject();
