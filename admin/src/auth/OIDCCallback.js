b427dc16fd449cb89629e123867e5c581fcdb1e0
cobusc <cobus.carstens@gmail.com>
2018-05-11 14:27:20 +0200

    State and nonce parameters are now generated per login request and both are checked upon redirect: the state in the URL and the nonce in the ID Token

--- admin/src/auth/OIDCCallback.js
+++ admin/src/auth/OIDCCallback.js
@@ -4,18 +4,52 @@ import queryString from 'query-string'
 
 class OIDCCallback extends Component {
     render () {
-        const auth_state = localStorage.getItem('auth_state');
         const parsed_query = queryString.parse(this.props.location.search);
+
+        // Check that the state returned in the URL matches the one stored.
+        const auth_state = localStorage.getItem('auth_state');
         if (auth_state !== parsed_query.state) {
             return (
                 <Redirect push to="/login" />
             )
         }
+
+        // Check that the nonce returned in the id token matches the one stored.
+        const segments = parsed_query.id_token.split('.');
+        if (segments.length !== 3) {
+            console.log("Token contains " + segments.length + " segments, but it should have 3.");
+            return (
+                <Redirect push to="/login" />
+            )
+        }
+
+        // All segment are base64 encoded
+        const payloadSeg = segments[1];
+        const payload = JSON.parse(base64urlDecode(payloadSeg));
+
+        const auth_nonce = localStorage.getItem('auth_nonce');
+        if (auth_nonce !== payload.nonce) {
+            console.log("Nonce mismatch: " + auth_nonce + " " + payload.nonce);
+            return (
+                <Redirect push to="/login" />
+            )
+        }
+
+        // Everything checked out. Store the id token.
         localStorage.setItem('id_token', parsed_query.id_token);
         return (
             <Redirect push to="/" />
         )
     }
+}
+
+function base64urlDecode(str) {
+  return new Buffer(base64urlUnescape(str), 'base64').toString();
+};
+
+function base64urlUnescape(str) {
+  str += Array(5 - str.length % 4).join('=');
+  return str.replace(/-/g, '+').replace(/_/g, '/');
 }
 
 export default OIDCCallback;
--- admin/src/auth/authLogin.js
+++ admin/src/auth/authLogin.js
@@ -17,25 +17,47 @@ const OIDC_PROVIDER_URL = process.env.REACT_APP_AUTHORIZATION_ENDPOINT;
 const OIDC_PROVIDER_SCOPE = 'openid profile roles';
 const OIDC_PROVIDER_CLIENT_ID = process.env.REACT_APP_CLIENT_ID;
 const OIDC_PROVIDER_REDIRECT_URI = process.env.REACT_APP_PORTAL_URL + process.env.REACT_APP_PORTAL_LOGIN_CALLBACK
-const OIDC_PROVIDER_STATE = btoa(new Date());
-const OIDC_PROVIDER_NONCE = btoa(OIDC_PROVIDER_STATE);
 
-const oidc_query_arguments = {
+const OIDC_QUERY_ARGUMENTS = {
     scope: `${OIDC_PROVIDER_SCOPE}`,
     response_type: `id_token token`,
     client_id: `${OIDC_PROVIDER_CLIENT_ID}`,
     redirect_uri: `${OIDC_PROVIDER_REDIRECT_URI}`,
-    state: `${OIDC_PROVIDER_STATE}`,
-    nonce: `${OIDC_PROVIDER_NONCE}`
 }
 
-const oidc_query_string = GenerateQueryString(oidc_query_arguments);
+/* The following link describes the difference between the state and nonce parameters nicely:
+https://stackoverflow.com/questions/46844285/difference-between-oauth-2-0-state-and-openid-nonce-parameter-why-state-cou
 
-const login_url = `${OIDC_PROVIDER_URL}?${oidc_query_string}`;
+To summarise:
+* the `state` parameter is used to protect against CSRF attacks
+* the `nonce` parameter is a token validation parameter, since the nonce will be contained in the token returned
+
+*/
+
+function generateNonce() {
+    const charset = '0123456789ABCDEFGHIJKLMNOPQRSTUVXYZabcdefghijklmnopqrstuvwxyz-._~'
+    const result = [];
+    window.crypto.getRandomValues(new Uint8Array(32)).forEach(c =>
+        result.push(charset[c % charset.length]));
+    return result.join('');
+}
 
 class AuthLoginPage extends Component {
     render() {
-        localStorage.setItem('auth_state', OIDC_PROVIDER_STATE);
+        let query_arguments = {};
+
+        Object.keys(OIDC_QUERY_ARGUMENTS).forEach(function(key) {
+             query_arguments[ key ] = OIDC_QUERY_ARGUMENTS[ key ];
+        });
+        query_arguments.state = btoa(new Date());
+        query_arguments.nonce = generateNonce();
+
+        // Store values so that they can be used for validation in OIDCCallback.js
+        localStorage.setItem('auth_state', query_arguments.state);
+        localStorage.setItem("auth_nonce", query_arguments.nonce);
+
+        const query_string = GenerateQueryString(query_arguments);
+        const login_url = `${OIDC_PROVIDER_URL}?${query_string}`;
         return (
             <MuiThemeProvider muiTheme={muiTheme}>
                 <div style={{ ...styles.main, backgroundColor: pink500 }}>
