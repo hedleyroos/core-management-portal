26c50169f9f39c4bb2e5823a20e3941a5cfef99e
David Oberholzer <david.oberholzer@praekeltconsulting.com>
2018-05-25 14:30:36 +0200

    Unused import removed.

--- admin/src/pages/ManageUserRoles/AssignRoleCard.js
+++ admin/src/pages/ManageUserRoles/AssignRoleCard.js
@@ -1,5 +1,4 @@
 import React from 'react';
-import PropTypes from 'prop-types';
 import Card from 'material-ui/Card/Card';
 import CardHeader from 'material-ui/Card/CardHeader';
 import CardText from 'material-ui/Card/CardText';
