0c832b787f305147f8818300dd0188902374c2bc
David Oberholzer <david.oberholzer@praekeltconsulting.com>
2018-07-06 11:40:17 +0200

    Prettier fix removed for generated code.

--- admin/src/swaggerRestServer.js
+++ admin/src/swaggerRestServer.js
@@ -37,30 +37,30 @@ const FILTER_LENGTHS = {
     users: {
         country: {
             min: 2,
-            max: 2
+            max: 2,
         },
         email: {
-            min: 3
+            min: 3,
         },
         first_name: {
-            min: 3
+            min: 3,
         },
         last_name: {
-            min: 3
+            min: 3,
         },
         msisdn: {
-            min: 3
+            min: 3,
         },
         nickname: {
-            min: 3
+            min: 3,
         },
         username: {
-            min: 3
+            min: 3,
         },
         q: {
-            min: 3
-        }
-    }
+            min: 3,
+        },
+    },
 };
 
 // These are default filters that were required for the
