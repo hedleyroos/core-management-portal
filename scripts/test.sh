c7164330d21e381a4b91cdd7551feb7f58dcb0dc
David Oberholzer <david.oberholzer@praekeltconsulting.com>
2018-09-17 11:46:30 +0200

    Test branch for build args

---
+++ scripts/test.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+REPO=girleffect/core-management-portal
+# Map "master" branch to "latest" tag. "develop" branch will have the "develop" tag.
+TAG=${TRAVIS_BRANCH/master/latest}
+# Tags may not contain slashes. Since git flow uses slashes as part of the branch name,
+# we replace all slashes with underscores, i.e. "release/1.0.0-alpha" becomes "release_1.0.0-alpha".
+TAG=${TAG//\//_}
+
+echo ${BUILD_ARG}
+# docker build ${BUILD_ARG} -t ${REPO}:${TAG} .
+# docker login -u="${DOCKER_USERNAME}" -p="${DOCKER_PASSWORD}"
+# docker push ${REPO}
