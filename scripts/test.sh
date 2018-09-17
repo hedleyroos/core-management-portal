4535287558a9acbb843ba7182e2e6f6d37a648ae
David Oberholzer <david.oberholzer@praekeltconsulting.com>
2018-09-17 11:51:57 +0200

    More echo lines

--- scripts/test.sh
+++ scripts/test.sh
@@ -7,7 +7,10 @@ TAG=${TRAVIS_BRANCH/master/latest}
 # we replace all slashes with underscores, i.e. "release/1.0.0-alpha" becomes "release_1.0.0-alpha".
 TAG=${TAG//\//_}
 
+echo "Testing build arguments"
 echo ${BUILD_ARG}
+echo "Testing repo"
+echo ${REPO}
 # docker build ${BUILD_ARG} -t ${REPO}:${TAG} .
 # docker login -u="${DOCKER_USERNAME}" -p="${DOCKER_PASSWORD}"
 # docker push ${REPO}
